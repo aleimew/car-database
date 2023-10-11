0fd404812ca01aeb0f01ac1a733974e29d486b8f
aleimew <alex@amoore.com>
2023-10-11 17:35:02 -0400

    Implemented second database with filter functionality

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -9,7 +9,8 @@ const Layout = (props) => {
     const [euSum, setEuSum] = useState(0);
     const [caSum, setCaSum] = useState(0);
 
-    const [regionHasChanged, setRegionHasChanged] = useState(true);
+    const [regionFilter, setRegionFilter] = useState('All');
+    const [modelFilter, setModelFilter] = useState('All');
 
     const lengthCutOff = props.data.length + 3;
 
@@ -59,6 +60,16 @@ const Layout = (props) => {
         }
     }, [])
 
+    const handleRegionFilter = (e) => {
+        setRegionFilter(e.target.value);
+        console.log(regionFilter);
+    }
+
+    const handleModelFilter = (e) => {
+        setModelFilter(e.target.value);
+        console.log(modelFilter);
+    }
+
     const PrintDataNames = () => {
         return (
             <div className='database_section'>
@@ -79,22 +90,65 @@ const Layout = (props) => {
         )
     }
 
-    return (
-        <div className="database_box">
-            CAR DATABASE
-            {PrintDataNames()}
+    const PrintDropDowns = () => {
+        return (
             <div>
-                {carData.map((car, index) => {
-                    return (
-                        <div key={index}>
-                            {PrintCarData(car)}
-                        </div>
-                    )
-                }
-                )}
+                Region Filter
+                <select onChange={handleRegionFilter}>
+                    <option value="All">All</option>
+                    <option value="US">US</option>
+                    <option value="EU">EU</option>
+                    <option value="CA">CA</option>
+                </select>
+
+                Model Filter
+                <select onChange={handleModelFilter}>
+                    <option value="All">All</option>
+                    <option value="A">A</option>
+                    <option value="B">B</option>
+                    <option value="C">C</option>
+                    <option value="D">D</option>
+                </select>
+            </div>
+        )
+    }
+
+    return (
+        <div>
+            <div className="database_box">
+                CAR DATABASE #1
+                {PrintDataNames()}
+                <div>
+                    {carData.map((car, index) => {
+                        return (
+                            <div key={index}>
+                                {PrintCarData(car)}
+                            </div>
+                        )
+                    }
+                    )}
+                </div>
             </div>
 
-
+            <div className="database_box">
+                CAR DATABASE #2
+                {PrintDropDowns()}
+                {PrintDataNames()}
+                <div>
+                    {carData.map((car, index) => {
+                        return (
+                            <div key={index}>
+                                {regionFilter === car.region || modelFilter === car.model ?
+                                    PrintCarData(car)
+                                    : regionFilter === 'All' && modelFilter === 'All' ?
+                                        PrintCarData(car)
+                                        : null}
+                            </div>
+                        )
+                    }
+                    )}
+                </div>
+            </div>
         </div>
     );
 };
